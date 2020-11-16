3dcfc14f8ff56b53fd1af6ed1351a2754791427c
Brock Sauvage <bsauvage14@gmail.com>
2020-11-15 23:24:46 -0600

    Style adjustments

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -7,8 +7,8 @@ class ContactsController < ApplicationController
   def create
     @contact = Contact.new(params[:contact])
     @contact.request = request
-    if ContactMailer.with(contact: @contact.to_json).hello_email.deliver_later
-      flash.now[:notice] = "Thank you for your message. I'll be getting in touch with you soon!"
+    if @contact
+      ContactMailer.with(contact: @contact).hello_email.deliver_now
     else
       flash.now[:alert] = "Your message didn't send! Get in touch with me at brocksauvage@gmail.com."
     end
